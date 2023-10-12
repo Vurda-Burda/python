ac72ecb374ef00a3890481389649538e007410e5
Aleksandr Bolokhov <bolohov@inbox.ru>
2023-10-12 17:51:51 +0300

    homework_10

---
+++ homework_10.py
@@ -0,0 +1,18 @@
+import random
+import pandas as pd
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+print(data.head())
+
+#Вариант с get_dummies
+print(pd.get_dummies(lst))
+
+#Вариант без get_dummies
+data['tmp'] = 1
+data.set_index([data.index, 'whoAmI'], inplace=True)
+data = data.unstack(level=-1, fill_value = 0).astype(int)
+data.columns = data.columns.droplevel()
+data.columns.name = None
+print(data)
